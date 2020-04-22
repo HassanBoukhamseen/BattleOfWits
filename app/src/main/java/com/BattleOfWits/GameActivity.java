87b72ded3141dab1509d2b6a0fe350a20a691878
tirth <tirthvp2@illinois.edu>
2020-04-22 03:00:22 -0500

    Major change to UI!!

--- app/src/main/java/com/BattleOfWits/GameActivity.java
+++ app/src/main/java/com/BattleOfWits/GameActivity.java
@@ -284,7 +284,7 @@ public class GameActivity extends HomeActivity {
                             Answer3.setText(answers.get(1));
                             Answer4.setText(answers.get(3));
 
-                            int ourColor = Color.parseColor("#FFFFBB33");
+                            int ourColor = Color.parseColor("#F5F2E4");
                             Answer1.setBackgroundTintList(ColorStateList.valueOf(ourColor));
                             Answer2.setBackgroundTintList(ColorStateList.valueOf(ourColor));
                             Answer3.setBackgroundTintList(ColorStateList.valueOf(ourColor));
